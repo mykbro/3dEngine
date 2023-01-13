f1d13f3b5af037fa0056198d83e1881242cc1336
Michele <Michele@MICHELE-PC>
2023-01-13 14:28:25 +0100

    removed PointToVector3 call and added some variable caching (like fragmentColor). Amazing boost !

--- Graphics/RenderTarget.cs
+++ Graphics/RenderTarget.cs
@@ -49,10 +49,9 @@ namespace _3dGraphics.Graphics
             Vector3 p2_p3 = p2 - p3;    //vector A in AxB
             Vector3 p1_p2 = p1 - p2;
             Vector3 p3_p1 = p3 - p1;
+            Vector3 p3_p2 = p3 - p2;
 
-            
-            //we project them on the Z=0 plane for the inside/outside check cross product
-            
+            //we project them on the Z=0 plane for the inside/outside check cross product            
             Vector3 projP2_P3 = ProjectTo2D(p2_p3);
             Vector3 projP1_P2 = ProjectTo2D(p1_p2);
             Vector3 projP3_P1 = ProjectTo2D(p3_p1);
@@ -60,47 +59,41 @@ namespace _3dGraphics.Graphics
             Vector3 projP1 = ProjectTo2D(p1);
             Vector3 projP2 = ProjectTo2D(p2);
             Vector3 projP3 = ProjectTo2D(p3);
-            
+           
+
+            Color fragmentColor = fragment.Color;
 
             for (int x = minX; x < maxX + 1; x++)   //x < maxX + 1 is equal to x <= maxX
             {
                 for(int y = minY; y < maxY + 1; y++)
                 {   
                     //we check if we're inside the screen
-                    bool pointInsideScreen = (0 <= x && x < Width && 0 <= y && y < Height);                    
+                    bool pointInsideScreen = (0 <= x && x < _width && 0 <= y && y < _height);                    
 
                     if (pointInsideScreen)
                     {
-                        Vector3 p = PointToVector3(new Point(x, y));    //already projected on Z=0 plane
+                        //Vector3 p = PointToVector3(new Point(x, y));    //already projected on Z=0 plane
+                        Vector3 p = new Vector3(x + 0.5f, y + 0.5f, 0f);
 
                         
-                        //we check if we're inside the triangle using cross products
-                        
+                        //we check if we're inside the triangle using cross products                        
                         Vector3 projP_P3 = p - projP3;
                         Vector3 projP_P2 = p - projP2;
-                        Vector3 projP_P1 = p - projP1;
-                        
-                        /*
-                        Vector3 a = Vector3.Cross(projP2_P3, projP_P3);
-                        Vector3 b = Vector3.Cross(projP1_P2, projP_P2);
-                        Vector3 c = Vector3.Cross(projP3_P1, projP_P1);
-                        
-                        bool pointInsideTriangle = (a.Z <= 0 && b.Z <= 0 && c.Z <= 0);
-                        */
+                        Vector3 projP_P1 = p - projP1;                        
+                    
 
                         bool pointInsideTriangle = (Vector3.Cross(projP2_P3, projP_P3).Z <= 0 &&    //early reject using && properties
                                                     Vector3.Cross(projP1_P2, projP_P2).Z <= 0 &&
                                                     Vector3.Cross(projP3_P1, projP_P1).Z <= 0);
                         
-                        
+
                         //bool pointInsideTriangle = PointInTriangle(p, p1, p2, p3);
 
                         if (pointInsideTriangle)
                         {
                             
                             //we interpolate the point Z using the plane equation (we use P2 and the norm (P1-P2 X P3-P2) to describe the triangle plane)
-                            //we then use the equation [(P1-P2 X P3-P2)]*(P-P2) = 0 to derive P.Z
-                            Vector3 p3_p2 = p3 - p2;
+                            //we then use the equation [(P1-P2 X P3-P2)]*(P-P2) = 0 to derive P.Z                           
                             Vector3 p_p2 = p - p2;
 
                             float interpolatedZ = -((p1_p2.Y * p3_p2.Z - p1_p2.Z * p3_p2.Y) * p_p2.X + (p1_p2.Z * p3_p2.X - p1_p2.X * p3_p2.Z) * p_p2.Y) / (p1_p2.X * p3_p2.Y - p1_p2.Y * p3_p2.X) + p2.Z;
@@ -108,7 +101,7 @@ namespace _3dGraphics.Graphics
                             //float interpolatedZ = (p1.Z + p2.Z + p3.Z) / 3;   //simple implementation
                             
                             //we calculate the pixel number
-                            int pixelNr = y * Width + x;
+                            int pixelNr = y * _width + x;
 
                             lock (_pixelLocks[pixelNr])
                             {
@@ -118,9 +111,9 @@ namespace _3dGraphics.Graphics
 
                                     int pixelStartingByte = pixelNr * Stride;
 
-                                    _data[pixelStartingByte] = fragment.Color.B;
-                                    _data[pixelStartingByte + 1] = fragment.Color.G;
-                                    _data[pixelStartingByte + 2] = fragment.Color.R;
+                                    _data[pixelStartingByte] = fragmentColor.B;
+                                    _data[pixelStartingByte + 1] = fragmentColor.G;
+                                    _data[pixelStartingByte + 2] = fragmentColor.R;
                                     //_data[pixelStartingByte + 3] = 0;   //alpha, we spare the write
                                 }
                             }                            
